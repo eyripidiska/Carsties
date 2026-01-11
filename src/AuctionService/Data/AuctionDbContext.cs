d4cab3c31cf147fa86c15220d75b40cafcbceb71
Evripidis Katsianos <euripidiskatsianos@gmail.com>
2026-01-11 18:08:38 +0200

    End of section 2

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace AuctionService.Data
+{
+    public class AuctionDbContext : DbContext
+    {
+        public AuctionDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+    }
+}
